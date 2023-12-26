d3a3b50dabc4440447a9b1a2ed939c345df97dcd
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2023-12-26 16:16:39 -0500

    Update Valid Parentheses.py

--- Easy Problems/Valid Parentheses.py
+++ Easy Problems/Valid Parentheses.py
@@ -5,12 +5,12 @@ class Solution:
 
         for char in s: # Iterates over each character 'char' in the string 's'. Each character is examined to determine whether it's an opening or closing bracket.
             if char in closeToOpen: # Checks if the current character is a closing bracket (i.e., one of the keys in the closeToOpen).
-                if stack and stack[-1] == closeToOpen[char]: ## Checks if the stack is not empty and checks whether the bracket at the top of the stack is the matching opening bracket 
+                if stack and stack[-1] == closeToOpen[char]: # Checks if the stack is not empty and checks whether the bracket at the top of the stack is the matching opening bracket 
                     stack.pop() # Removes the last opening bracket from the stack
                 else:
                     return False
             else:
-                stack.append(char) ## If the current character is not a closing bracket, it's treated as an opening bracket and is added to the stack. This is for later matching with a corresponding closing bracket.
+                stack.append(char) # If the current character is not a closing bracket, it's treated as an opening bracket and is added to the stack. This is for later matching with a corresponding closing bracket.
         return True if not stack else False # After processing all characters in the string, this line is executed. True if the stack is empty, indicating that all opening brackets have been properly matched with closing brackets. False, if the stack is not empty, it means there are unmatched opening brackets left
             
 
