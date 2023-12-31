9ff188ce4bdfdf1cf7aa86d670e7fccdc2bd85ca
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2023-12-31 11:11:50 -0500

    Update Remove Duplicates from Sorted Array.py

--- Easy Problems/Remove Duplicates from Sorted Array.py
+++ Easy Problems/Remove Duplicates from Sorted Array.py
@@ -1,9 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-        l = 1 #Keep track of the position in the list nums
+        l = 1 #Keep track of the position in the list nums (left)
 
-        for r in range(1,len(nums)): # Iterate over the indices of the list nums, starting from index 1 (the second element) to the last index. The variable r represents the current index in the loop.
+        for r in range(1,len(nums)): # Iterate over the indices of the list nums, starting from index 1 (the second element) to the last index. The variable r (right) represents the current index in the loop.
             if nums[r] != nums[r-1]: #Checks if the current element (nums[r]) is different from the previous element 
                 nums[l] = nums[r] #Assign the value of nums[r] to nums[l]
                 l += 1 #To move the position for the next unique element.
