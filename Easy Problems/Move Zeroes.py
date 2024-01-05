a9997cc1aec85d4f8235425aaa7b590d78bf5c27
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-01-05 10:12:33 -0500

    Update Move Zeroes.py

--- Easy Problems/Move Zeroes.py
+++ Easy Problems/Move Zeroes.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = 0 #Initializes a variable l (left pointer) to 0. It is used to track the position where a non-zero element should be placed
+        l = 0 #Initializes a variable l (left pointer) to 0. It tracks the position where a non-zero element should be placed
         for r in range(len(nums)): #r is a variable that takes values from 0 to the length of the list nums minus one. It serves as the right pointer, moving through each element of the list
             if nums[r]: #Checks if the current element at index r is non-zero
                 nums[l], nums[r] = nums[r], nums[l] #If the current element is non-zero, it is swapped with the element at index l. This effectively moves the non-zero element to the front part of the list
