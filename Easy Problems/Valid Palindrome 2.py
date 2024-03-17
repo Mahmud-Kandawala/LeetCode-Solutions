6697023bf29b2fcc0cbc9cfebce4969782268907
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-03-16 23:40:26 -0400

    Update Valid Palindrome 2.py

--- Easy Problems/Valid Palindrome 2.py
+++ Easy Problems/Valid Palindrome 2.py
@@ -1,1 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome(s, left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left, right = left + 1, right - 1
+            return True
 
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                skipL = is_palindrome(s, l + 1, r)
+                skipR = is_palindrome(s, l, r - 1)
+                return skipL or skipR
+            l, r = l + 1, r - 1
+        
+        return True
