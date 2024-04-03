651d95fd8032a30675c91141c2f66840b7fd129d
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-04-02 20:25:12 -0400

    Update Contains Duplicates 2.py

--- Easy Problems/Contains Duplicates 2.py
+++ Easy Problems/Contains Duplicates 2.py
@@ -1,1 +1,13 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set() #Used to store the unique elements from nums that are within the current window of size k
+        L = 0
 
+        for R in range(len(nums)):
+            if R - L > k: #Checks if the current window size is greater than k. If it is, it means we need to shrink the window from the left
+                window.remove(nums[L]) # removes the leftmost element from window because it's no longer within the distance k from the current element
+                L += 1 # increments L, effectively moving the left boundary of the window to the right.
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False #If the method completes the loop without finding any duplicates within the specified distance, it returns False
