7a720883e0ca79c628687964c1babe8d49a06839
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-05-03 09:56:26 -0400

    Update Best Time to Buy and Sell Stock.py

--- Easy Problems/Best Time to Buy and Sell Stock.py
+++ Easy Problems/Best Time to Buy and Sell Stock.py
@@ -1,1 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1 #Left buy (Day 1), Right sell (Day 2)
+        maxP = 0 #Keep track of the maximum profit observed
+
+        while r < len(prices): #Loop iterates through 'prices'. Checks that r is within the bounds
+            if prices[l] < prices[r]: #If the price on day l is less than the price on day r
+                profit = prices[r] - prices [l]
+                maxP = max(maxP, profit)
+            else:
+                l = r #Updates the l pointer to the current position of r because it would be better to consider buying at a lower price
+            r += 1 #Pointer is incremented to consider the next day as a potential day to sell the stock
+        
+        return maxP #Returns the maximum profit 
+
+
 
