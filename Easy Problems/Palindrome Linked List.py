f1aae4c153c69e9e258a4d187c366dc5d18a6952
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-06-05 19:06:40 -0400

    Update Palindrome Linked List.py

--- Easy Problems/Palindrome Linked List.py
+++ Easy Problems/Palindrome Linked List.py
@@ -1,1 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        
 
+        while fast and fast.next: #Not None/Null
+            fast = fast.next.next
+            slow = slow.next
+
+       
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+
+ 
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+
+
+
+
+        
+        
