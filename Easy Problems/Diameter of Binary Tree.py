104cdfd392fbe02229414fe897acec68146b60a8
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-06-20 10:06:57 -0400

    Update Diameter of Binary Tree.py

--- Easy Problems/Diameter of Binary Tree.py
+++ Easy Problems/Diameter of Binary Tree.py
@@ -1,1 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        res = [0] # A list with a single element 0. This will store the maximum diameter found during the traversal of the tree. A list is used to allow updates within the nested function.
+        
+        def dfs(root): # Helper Function
+            if not root: # If Null
+                return -1
+            left = dfs(root.left) # Recursively calls itself for the left and right children of the current node.
+            right = dfs(root.right)
 
+            res[0] = max(res[0], 2 + left + right)# Diameter through the current node is calculated as 2 + left + right
+
+            return 1 + max(left, right) # Returns the height of the current node
+
+        dfs(root)
+        return res[0]
